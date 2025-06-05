e06c93f9a834a6c467cb017376ab6b61ea9b62d8
Abdulrahmanaak <Abdulrahman1aak@gmail.com>
2025-06-05 14:43:23 +0300

    Bundle fonts locally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,27 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = localFont({
+  src: [
+    {
+      path: "../public/fonts/GeistVariable.woff2",
+      weight: "100 900",
+    },
+  ],
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  display: "swap",
 });
 
-const geistMono = Geist_Mono({
+const geistMono = localFont({
+  src: [
+    {
+      path: "../public/fonts/GeistMonoVariable.woff2",
+      weight: "100 900",
+    },
+  ],
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
